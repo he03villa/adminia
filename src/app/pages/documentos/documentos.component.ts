9f73f71a775abfb48750c30855b00044c46ee9d4
he03villa <he03villa@gmail.com>
2022-06-14 17:39:06 -0500

    implementando

--- src/app/pages/documentos/documentos.component.ts
+++ src/app/pages/documentos/documentos.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-documentos',
   templateUrl: './documentos.component.html',
-  styleUrls: ['./documentos.component.css']
+  styleUrls: ['./documentos.component.scss']
 })
 export class DocumentosComponent implements OnInit {
 

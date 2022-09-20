e5b140a72a9163bd2c5c5b62d03b70a70e1489db
he03villa <he03villa@gmail.com>
2022-09-19 22:21:43 -0500

    arreglando

---
+++ api/controllers/revision/updateTipoRevicion.php
@@ -0,0 +1,12 @@
+<?php
+
+    include '../cors.php';
+    include '../error.php';
+    require '../../models/revision.php';
+    $datos = json_decode(file_get_contents("php://input"),TRUE);
+    $post = $_POST;
+    if ($datos == null) $reques = $post;
+    else $reques = $datos;
+    $revision = new Revision();
+    $res = $revision->updateTipoRevicion($reques);
+    echo json_encode($res);
--- src/app/pages/documentos/documentos.component.ts
+++ src/app/pages/documentos/documentos.component.ts
@@ -3,6 +3,7 @@ import { ServicesService } from '../../services/services.service';
 import { RevicionService } from '../../services/revicion.service';
 import { PropiedadService } from '../../services/propiedad.service';
 import { DashboardComponent } from '../dashboard/dashboard.component';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-documentos',
@@ -35,15 +36,15 @@ export class DocumentosComponent implements OnInit {
   }
 
   async cargarListarTipo() {
-    const data = { tipo: [1, 2] }
+    const user = JSON.parse(localStorage.getItem('dataUser'));
+    const propiedad = JSON.parse(sessionStorage.getItem('dataPropiedad'));
+    const data = { cojunto: propiedad ? propiedad.cojunto_id : user.id };
     const res:any = await this.Revicion.getAllTipoRevision(data);
-    console.log(res);
     this.arrayTipoRevision = res.data;
   }
 
   async listarPropietarios(propiedad) {
     const res:any = await this.Propiedad.getAllPropiedad({ id: propiedad });
-    console.log(res);
     this.arrayPropietarios = res.data;
   }
 
@@ -57,14 +58,12 @@ export class DocumentosComponent implements OnInit {
       const user = JSON.parse(localStorage.getItem('dataUser'));
       const data  = { cojunto: user.id, tipo_revision: item.id };
       const res:any = await this.Revicion.getRevisionPropiedad(data);
-      console.log(res);
       this.documento = true;
       this.arrayDocumento = res.data;
     } else {
       const propiedad = JSON.parse(sessionStorage.getItem('dataPropiedad'));
       const data  = { propiedad: propiedad.id, tipo_revision: item.id };
       const res:any = await this.Revicion.getRevision(data);
-      console.log(res);
       this.documento = true;
       this.arrayDocumento = res.data;
     }
@@ -73,6 +72,83 @@ export class DocumentosComponent implements OnInit {
   atras() {
     this.arrayDocumento = [];
     this.documento = false;
+  }
+
+  async abrirModalCrearCarpeta(item) {
+    let data;
+    if (!item) {
+      data = { nombre: '', descripcion: '' };
+    } else {
+      data = item;
+    }
+    console.log(data);
+    let html = `
+      <div class="group-form">
+        <label>Nombre</label>
+        <input type="text" id="carpeta-nombre" value="${ data.nombre }">
+      </div>
+      <div class="group-form">
+        <label>Descripción</label>
+        <input type="text" id="carpeta-descripcion" value="${ data.descripcion }">
+      </div>
+    `;
+    const dato:any = await Swal.fire({
+      title: 'Carpeta',
+      html: html,
+      showCancelButton: true,
+      confirmButtonText: 'Guardar',
+      cancelButtonText: 'Cancelar',
+      backdrop: true,
+      customClass: {
+        htmlContainer: 'content-group-form'
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+      preConfirm: async () => {
+        const nombre = document.getElementById('carpeta-nombre')['value'];
+        const descripcion = document.getElementById('carpeta-descripcion')['value'];
+        if (!this.services.validarText(nombre)) {
+          Swal.showValidationMessage(`El nombre está vacío`);
+        } else if (!this.services.validarText(descripcion)) {
+          Swal.showValidationMessage(`La descripción está vacío`);
+        }
+        const user = JSON.parse(localStorage.getItem('dataUser'));
+        const datos = { nombre, descripcion, cojunto: user.id };
+        let res:any;
+        if (!item) {
+          res = await this.Revicion.saveTipoRevicion(datos);
+        } else {
+          datos['id'] = data.id;
+          res = await this.Revicion.updateTipoRevicion(datos);
+        }
+        if (res.status == 'success') {
+          return res;
+        } else {
+          Swal.showValidationMessage(res.message);
+        }
+      }
+    });
+    if (dato.isConfirmed) {
+      this.services.Alert(dato.value.status, '', dato.value.message, 'Aceptar', '');
+      if (!item) {
+        this.arrayTipoRevision.unshift(dato.value.data);
+      } else {
+        const post = this.arrayTipoRevision.findIndex(f => f.id == dato.value.data.id);
+        this.arrayTipoRevision[post] = dato.value.data;
+      }
+    }
+  }
+
+  async deleteCarpeta(item) {
+    const res:any = await this.services.Alert('info', '', 'Esta seguro en eliminar esta carpeta', 'Aceptar', 'Canselar', true);
+    if (res.isConfirmed) {
+      const data = { id: item.id };
+      const resp:any = await this.Revicion.deleteTipoRevicion(data);
+      this.services.Alert(resp.status == 'success' ? 'success' : 'error', '', resp.message, 'Aceptar', '');
+      if (resp.status == 'success') {
+        const post = this.arrayTipoRevision.findIndex(f => f.id == data.id);
+        this.arrayTipoRevision.splice(post, 1);
+      }
+    }
   }
 
 }

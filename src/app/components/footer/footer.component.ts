562ad64a76280bf7c32dd059a3fd13d8b7d436db
he03villa <he03villa@gmail.com>
2022-11-28 17:21:34 -0500

    fix

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss']
+})
+export class FooterComponent implements OnInit {
+
+  fecha = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.fecha = new Date().getFullYear();
+  }
+
+}
